adfb6154f21578c84a4972ac60cdce2bca815313
Duncan Malashock <duncanmalashock@gmail.com>
2026-09-14 21:19:49 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<h1 class="flex items-center bg-blue-500 px-4 text-white">
+  Welcome to SvelteKit
+</h1>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
